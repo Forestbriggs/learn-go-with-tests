34a2fd2caf499c2c23e00602b97ca18b6d516831
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-10-11 18:55:54 -0700

    finished minimum SumAll

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"golang.org/x/exp/slices"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +17,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hello
 
 go 1.23.1
+
+require golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
