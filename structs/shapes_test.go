c19c527b3ebd8f34df6f52cd927953867fa68ea6
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-10-16 15:41:32 -0700

    clean up commit

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -23,9 +23,9 @@ func TestArea(t *testing.T) {
 		shape   Shape
 		hasArea float64
 	}{
-		{shape: Rectangle{12, 6}, hasArea: 72},
-		{shape: Circle{10}, hasArea: 314.1592653589793},
-		{shape: Triangle{12, 6}, hasArea: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
