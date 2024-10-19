815bff7d7cb2c330a3f9087f855093e5ab5ad132
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-10-18 21:47:35 -0700

    wrote min passing code for Add

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -15,6 +15,6 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, defintion string) {
-
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
