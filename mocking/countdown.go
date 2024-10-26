fc75d1246fd1bd06422f378b8eb6284cde0fe3ae
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-10-26 16:21:42 -0700

    made Sleeper interface and SpySleeper to mock time.Sleep in function

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,6 +10,10 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
+type Sleeper interface {
+	Sleep()
+}
+
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -5,10 +5,19 @@ import (
 	"testing"
 )
 
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	SpySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, SpySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -18,5 +27,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if SpySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", SpySleeper.Calls)
 	}
 }
