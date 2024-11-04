382d0af4c195d8fee1df2fdf29fa81bc2f94c61f
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-11-03 16:14:03 -0800

    fixed test and wc

--- concurrency/v0/websiteChecker_test.go
+++ concurrency/v0/websiteChecker_test.go
@@ -6,10 +6,6 @@ import (
 	"time"
 )
 
-func mockWebsiteChecker(url string) bool {
-	return url != "waat://furhurterwe.geds"
-}
-
 func slowStubWebsiteChecker(_ string) bool {
 	time.Sleep(20 * time.Millisecond)
 	return true
@@ -26,17 +22,21 @@ func BenchmarkCheckWebsites(b *testing.B) {
 	}
 }
 
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
 func TestCheckWebsites(t *testing.T) {
 	websites := []string{
 		"http://google.com",
-		"http://blog.dave5.com",
+		"http://blog.gypsydave5.com",
 		"waat://furhurterwe.geds",
 	}
 
 	want := map[string]bool{
-		"http://google.com":       true,
-		"http://blog.dave5.com":   true,
-		"waat://furhurterwe.geds": false,
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
 	}
 
 	got := CheckWebsites(mockWebsiteChecker, websites)
