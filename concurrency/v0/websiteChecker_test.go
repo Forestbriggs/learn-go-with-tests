6eb97ca3d0d97fce654c6d83eef69f99e698dade
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-11-03 16:01:32 -0800

    made benchmark

--- concurrency/v0/websiteChecker_test.go
+++ concurrency/v0/websiteChecker_test.go
@@ -3,10 +3,27 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
 	return url != "waat://furhurterwe.geds"
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
 }
 
 func TestCheckWebsites(t *testing.T) {
